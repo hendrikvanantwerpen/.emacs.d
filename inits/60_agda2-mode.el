27dafc6181158924e61a3332e7236f010cb9c70d
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2018-05-16 15:28:34 +0200

    Update agda-mode loading.

--- inits/60_agda2-mode.el
+++ inits/60_agda2-mode.el
@@ -1,5 +1,6 @@
 (if (executable-find "agda-mode")
-    (load (shell-command-to-string "agda-mode locate")))
+  (load-file (let ((coding-system-for-read 'utf-8))
+                  (shell-command-to-string "agda-mode locate"))))
 (when (require 'agda2-mode nil t)
   (defun my-agda2-mode-hook ()
     "Modify keys and input-mode for agda mode"
