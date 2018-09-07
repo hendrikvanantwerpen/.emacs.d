97a4554e19aefbe54bc38cedfe19066bce5c5f82
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2018-09-07 13:55:47 +0200

    Remove deprecated Agda stdlib loading code.

--- init-sphaeramundi.el
+++
@@ -1,7 +0,0 @@
-(eval-after-load 'agda2
-  (let* ((agda-mode-path (shell-command-to-string "agda-mode locate"))
-         (agda-stdlib-path (replace-regexp-in-string "\/share.*$" "/lib/agda/src" agda-mode-path)))
-    (if (file-exists-p agda-stdlib-path)
-        (setq agda2-include-dirs
-              (list "." agda-stdlib-path))
-      (error "Agda stdlib directory %s not found." agda-stdlib-path))))
