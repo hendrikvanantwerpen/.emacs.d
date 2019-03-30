1e2c3e5948e41dbeb0b43ff270cf38ffd0fa6bc6
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2019-03-30 13:10:22 +0100

    Support more spell checkers.

--- el-get-user/inits/init-flyspell.el
+++ el-get-user/inits/init-flyspell.el
@@ -1,2 +1,11 @@
-(setq ispell-program-name "hunspell")
-(setq ispell-dictionary "en_US-large")
+(setq ispell-silently-savep t)
+(cond
+  ((executable-find "aspell")
+    (setq ispell-program-name "aspell")
+    (setq ispell-extra-args '("--sug-mode=ultra" "--lang=en_US-large")))
+  ((executable-find "hunspell")
+    (setq ispell-program-name "hunspell")
+    (setq ispell-really-hunspell t)
+    (setq ispell-local-dictionary "en_US")
+    (setq ispell-local-dictionary-alist
+          '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil ("-d" "en_US-large") nil utf-8)))))
