395feced66fd4a2e44d2e1a9f0ceaa9c1c5e6fcf
Hendrik van Antwerpen <hendrikvanantwerpen@github.com>
2021-09-30 12:32:32 +0200

    Add init for new host.

--- init.el
+++ init.el
@@ -24,7 +24,6 @@
         ac-python
         amd-mode
         auctex
-        auctex-latexmk
         auto-complete
         auto-complete-auctex
         ;auto-complete-css
@@ -36,7 +35,6 @@
         coffee-mode
         ebib
         el-get
-        ensime
         erc
         evil
         evil-org-mode
@@ -101,5 +99,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (load-dir)))
- '(safe-local-variable-values (quote ((eval latex-pretty-symbols-mode t)))))
+ '(package-selected-packages '(load-dir))
+ '(safe-local-variable-values '((eval latex-pretty-symbols-mode t))))
